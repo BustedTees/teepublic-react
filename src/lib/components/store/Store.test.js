514c766304e9f133a9d1a68849985e3efc03aa4d
Marcelo <marcelo@teepublic.com>
2018-04-06 12:30:03 -0400

    update test configuration

--- src/lib/components/store/Store.test.js
+++ src/lib/components/store/Store.test.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Store from './Store';
 
+import { ROUTE_CONFIGURATION } from '../../../demo/RouteConfiguration';
+import store from '../../../demo/StoreData';
+
 it('Store renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Store />, div);
+  ReactDOM.render(
+    <Store
+      storeData={store}
+      buyProductLinkBuilder={() => {
+        '';
+      }}
+      configuration={ROUTE_CONFIGURATION}
+    />,
+    div
+  );
 });
