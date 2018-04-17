844fe46abeb9b4a01d102b9d2702d3a41d895546
Jeff Cooper <jeffcoperjc@gmail.com>
2018-04-17 15:17:48 -0400

    Build updates.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 export {
+  default as GlobalStyles
+} from './lib/components/global_styles/GlobalStyles';
+
+export { default as AddToCart } from './lib/components/add_to_cart/AddToCart';
+export {
+  default as BackToProducts
+} from './lib/components/back_to_products/BackToProducts';
+export { default as BuyProduct } from './lib/components/buy_product/BuyProduct';
+export { default as Cart } from './lib/components/cart/Cart';
+export { default as CartButton } from './lib/components/cart_button/CartButton';
+export { default as CartItem } from './lib/components/cart_item/CartItem';
+
+export {
   default as DesignCollection
 } from './lib/components/design_collection/DesignCollection';
+
+export { default as DesignTile } from './lib/components/design_tile/DesignTile';
+export { default as Footer } from './lib/components/footer/Footer';
 export {
-  default as TeePublicPowered
-} from './lib/components/teepublic_powered/TeepublicPowered';
-export { default as Cart } from './lib/components/cart/Cart';
-export { default as BuyProduct } from './lib/components/buy_product/BuyProduct';
-export { default as CartButton } from './lib/components/cart_button/CartButton';
+  default as ImageGallery
+} from './lib/components/image_gallery/ImageGallery';
+export { default as Pagination } from './lib/components/pagination/Pagination';
+export {
+  default as ProductVariants
+} from './lib/components/product_variants/ProductVariants';
+export {
+  default as RelatedTags
+} from './lib/components/related_tags/RelatedTags';
+export {
+  default as SkuSelector
+} from './lib/components/sku_selector/SkuSelector';
+export { default as Store } from './lib/components/store/Store';
+
 export {
   default as StoreFilter
 } from './lib/components/store_filter/StoreFilter';
-export { default as Pagination } from './lib/components/pagination/Pagination';
-export { default as Store } from './lib/components/store/Store';
-export { default as Footer } from './lib/components/footer/Footer';
+
+export {
+  default as TeePublicPowered
+} from './lib/components/teepublic_powered/TeepublicPowered';
